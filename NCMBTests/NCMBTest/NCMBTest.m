1d4a41410691c1b9a5538a473c4bd9c96a6118c5
ncmbsdk <ncmb_sdk@list.nifty.co.jp>
2015-04-13 18:36:15 +0900

    [Remove] Removed test script

--- NCMBTests/NCMBTest/NCMBTest.m
+++ NCMBTests/NCMBTest/NCMBTest.m
@@ -1,84 +1,15 @@
-//
-//  NCMBTest.m
-//  NCMBTest
-//
-//  Created by SCI01433 on 2015/04/06.
-//  Copyright (c) 2015年 NIFTY Corporation. All rights reserved.
-//
-
-#import <Specta/Specta.h> // #import "Specta.h" if you're using libSpecta.a
-
-SharedExamplesBegin(MySharedExamples)
-// Global shared examples are shared across all spec files.
-
-sharedExamplesFor(@"foo", ^(NSDictionary *data) {
-    __block id bar = nil;
-    beforeEach(^{
-        bar = data[@"bar"];
-    });
-    it(@"should not be nil", ^{
-        //XCTAssertNotNil(bar);
-        XCTAssertNil(bar);
-    });
-});
-
-SharedExamplesEnd
-
-SpecBegin(Thing)
-
-describe(@"Thing", ^{
-    sharedExamplesFor(@"another shared behavior", ^(NSDictionary *data) {
-        // Locally defined shared examples can override global shared examples within its scope.
-    });
-    
-    beforeAll(^{
-        // This is run once and only once before all of the examples
-        // in this group and before any beforeEach blocks.
-    });
-    
-    beforeEach(^{
-        // This is run before each example.
-    });
-    
-    it(@"should do stuff", ^{
-        // This is an example block. Place your assertions here.
-    });
-    
-    it(@"should do some stuff asynchronously", ^{
-        waitUntil(^(DoneCallback done) {
-            // Async example blocks need to invoke done() callback.
-            done();
-        });
-    });
-    
-    itShouldBehaveLike(@"a shared behavior", @{@"key" : @"obj"});
-    
-    itShouldBehaveLike(@"another shared behavior", ^{
-        // Use a block that returns a dictionary if you need the context to be evaluated lazily,
-        // e.g. to use an object prepared in a beforeEach block.
-        return @{@"key" : @"obj"};
-    });
-    
-    describe(@"Nested examples", ^{
-        it(@"should do even more stuff", ^{
-            // ...
-        });
-    });
-    
-    pending(@"pending example");
-    
-    pending(@"another pending example", ^{
-        // ...
-    });
-    
-    afterEach(^{
-        // This is run after each example.
-    });
-    
-    afterAll(^{
-        // This is run once and only once after all of the examples
-        // in this group and after any afterEach blocks.
-    });
-});
-
-SpecEnd
+/*
+ Copyright 2014 NIFTY Corporation All Rights Reserved.
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
