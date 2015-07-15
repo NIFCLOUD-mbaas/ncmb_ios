442bb4b6d0c4b4be7a855278f6b9e755e22247df
rebirthble <0k4w4.yuk1@gmail.com>
2015-07-15 18:39:37 +0900

    added initial test code

--- NCMBTests/CoreTest/CoreTest.m
+++ NCMBTests/CoreTest/CoreTest.m
@@ -6,35 +6,42 @@
 //  Copyright (c) 2015年 NIFTY Corporation. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-#import <XCTest/XCTest.h>
+#import <Specta/Specta.h>
+#import <Expecta/Expecta.h>
+#import <OHHTTPStubs/OHHTTPStubs.h>
 
-@interface CoreTest : XCTestCase
+SpecBegin(StubTest)
 
-@end
-
-@implementation CoreTest
-
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
+beforeAll(^{
+    
+    NSDictionary *responseDic = @{@"key":@"value"};
+    NSData *jsonResponse = [NSJSONSerialization dataWithJSONObject:responseDic options:0 error:nil];
+    
+    //CreateStub
+    // Objective-C
+    [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+        return [request.URL.absoluteString isEqualToString:@"https://example.com/2013-09-01/classes/Test"];
+    } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+        return [OHHTTPStubsResponse responseWithData:jsonResponse
+                                          statusCode:200
+                                             headers:@{@"Content-Type":@"application/json"}];
     }];
-}
+});
 
-@end
+describe(@"OHHTTPStubs", ^{
+    it(@"should return stub response", ^{
+        NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"https://example.com/2013-09-01/classes/Test"]];
+        NSHTTPURLResponse *response = nil;
+        NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:nil];
+        expect([NSJSONSerialization JSONObjectWithData:responseData
+                                               options:NSJSONReadingAllowFragments
+                                                 error:nil]
+               ).to.equal(@{@"key":@"value"});
+    });
+});
+
+afterAll(^{
+    [OHHTTPStubs removeAllStubs];
+});
+
+SpecEnd
